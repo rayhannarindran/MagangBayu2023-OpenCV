1ffe27f52c0815aa2dfdfec0165184ee17c145d6
kisenaa <johannesdanis304@gmail.com>
2023-02-11 20:56:50 +0700

    Johannes Daniswara Pratama_5025221276_TugasOpenCV

--- Johannes Daniswara Pratama_5025221276_TugasOpenCV/Tugas1/Johannes Daniswara Pratama_5025221276_Tugas1.py
+++ Johannes Daniswara Pratama_5025221276_TugasOpenCV/Tugas1/Johannes Daniswara Pratama_5025221276_Tugas1.py
@@ -1,12 +1,12 @@
 import cv2 as cv
 import numpy as np
 
-# Load and resize image
-img = cv.imread('tugas1.jpg')
-img = cv.resize(img, (640, 360))
+# Johannes Daniswara Pratama - 5025221276
 
-# Divide the image into 4 equal parts
-height, width = img.shape[:2]
+# Load and resize image
+img = cv.imread("Johannes Daniswara Pratama_5025221276_TugasOpenCV/Tugas1/tugas1.jpg")
+img = cv.resize(img, (1200, 674))
+
 # Split into vertical part
 split1, split2, = np.vsplit(img, 2)
 
@@ -14,16 +14,10 @@ split1, split2, = np.vsplit(img, 2)
 part1, part2 = np.hsplit(split1, 2)
 part3, part4 = np.hsplit(split2, 2)
 
-# Create a green color mask
+# Create a green, red, yellow, and blue color mask
 mask1 = np.full(part1.shape, (0, 255, 0), dtype=np.uint8) # Green color
-
-# Create a red color mask
 mask2 = np.full(part1.shape, (0, 0, 255), dtype=np.uint8)# Red color
-
-# Create a yellow color mask
 mask3 = np.full(part1.shape, (0, 255, 255), dtype=np.uint8) # Yellow color
-
-# Create a blue color mask
 mask4 = np.full(part1.shape, (255, 0, 0), dtype=np.uint8) # Blue color
 
 # Apply the green mask to all part with thickness of 0.7
@@ -37,6 +31,8 @@ result4 = cv.addWeighted(part4, 1, mask4, 0.7, 0)
 result = np.concatenate((np.concatenate((result1, result2), axis=1), np.concatenate((result3, result4), axis=1)), axis=0)
 
 # Show the resulting image
-cv.imshow('Result', result)
+cv.imshow('shape', result)
+# Save the image
+cv.imwrite("Johannes Daniswara Pratama_5025221276_TugasOpenCV/Tugas1/tugas1_done.jpg",result)
 cv.waitKey(0)
 cv.destroyAllWindows()
